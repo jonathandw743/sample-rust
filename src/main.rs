af92239a89a76f095a1be41e0d0e8682a2e2419c
Jonathan Wilson <85790952+jonathandw743@users.noreply.github.com>
2023-06-13 18:57:44 +0100

    greetings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+enum Language {
+    English,
+    French,
+    Chinese,
+}
+
+fn say_hello(name: &str, language: Language) {
+    if let Language::English = language {
+        println!("Hello {}!", name);
+    } else {
+        println!("Sorry, I don't speak that language.")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    say_hello("Jonathan", Language::English);
 }
